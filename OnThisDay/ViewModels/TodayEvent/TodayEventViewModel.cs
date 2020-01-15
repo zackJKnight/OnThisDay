f1d3aab04091219b3904b14297a957a1028926d0
zackJKnight <zachary.j.knight@gmail.com>
2020-01-14 22:37:24 -0600

    Remove unnecessary methods.

--- OnThisDay/ViewModels/TodayEvent/TodayEventViewModel.cs
+++ OnThisDay/ViewModels/TodayEvent/TodayEventViewModel.cs
@@ -8,29 +8,14 @@ namespace OnThisDay.ViewModels.TodayEvent
 {
     public class TodayEventViewModel : ViewModelBase
     {
-        private string _name;
 
-        public string Name
-        {
-            get { return _name; }
-            set { Set(() => Name, ref _name, value); }
-        }
+        public string Name { get; set; }
         public string Description { get; set; }
         public string Detail { get; set; }
         public TodayEventViewModel()
         {
-            //SetPropertyValues();
         }
 
-        private async void SetPropertyValues()
-        {
-            this.Name = await GetNameFromService();
-        }
-
-        private async Task<string> GetNameFromService()
-        {
-            return await new Task<string>(() => "hi");        
-        }
     }
 
 }
