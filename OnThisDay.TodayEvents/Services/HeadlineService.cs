e869eb5246fca6084588abe34c186b36d3f74bd7
Zack Knight <zachary.j.knight@gmail.com>
2020-04-18 15:07:50 -0500

    pick Add secret management to startup.

---
+++ OnThisDay.TodayEvents/Services/HeadlineService.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Storage.Blobs;
+using Grpc.Core;
+using Newtonsoft.Json;
+using OnThisDay.TodayEventData.Models;
+using OnThisDay.TodayEvents.Protos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OnThisDay.TodayEvents.Services
+{
+    public class HeadlineService : Protos.HeadlineService.HeadlineServiceBase
+    {
+        private HttpClient httpClient;
+
+        public HeadlineService(IHttpClientFactory httpClientFactory)
+        {
+            httpClient = httpClientFactory.CreateClient();
+
+        }
+
+        public async override Task<DownloadResponse> DownloadHeadlinesAsync(DownloadRequest request, ServerCallContext context)
+        {
+            int articlesFromYear = 1980;
+            var currentMonth = DateTime.Now.Month.ToString();
+
+            var webRequest = await httpClient.GetAsync($"https://api.nytimes.com/svc/archive/v1/{articlesFromYear}/{currentMonth}.json?api-key={Startup.NytApiKey}");
+
+            var content = webRequest.Content;
+            var response = new DownloadResponse();
+            var headlines = new List<TodayEventData.Models.Headline>();
+
+            try
+            {
+                var jsonContent = JsonConvert.DeserializeObject<NYTDocs>(await content.ReadAsStringAsync());
+
+                foreach (var headline in jsonContent.Response.Docs.Select(doc => doc.Headline))
+                {
+
+                    headlines.Add(headline);
+                }
+            }
+            catch (JsonSerializationException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            response.Headlines.AddRange(headlines.Select(Protos.Headline.FromRepositoryModel));
+            return response;
+        }
+    }
+}
--- OnThisDay.TodayEvents/Startup.cs
+++ OnThisDay.TodayEvents/Startup.cs
@@ -7,21 +7,42 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using OnThisDay.TodayEvents.Services;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace OnThisDay.TodayEvents
 {
-    class Startup
+    public class Startup
     {
+        public static string NytApiKey {get; private set;}
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ITodayEventRepository, TodayEventRepository>();
             services.AddGrpc();
         }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            var builder = new ConfigurationBuilder();
             if (env.IsDevelopment())
             {
+                builder.AddUserSecrets<Startup>();
+                NytApiKey = Configuration["API_KEY_NYT"];
                 app.UseDeveloperExceptionPage();
             }
 
